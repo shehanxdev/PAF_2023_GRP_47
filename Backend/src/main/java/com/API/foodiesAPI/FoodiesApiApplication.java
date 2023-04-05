385bfa9c44f17145e5361b374320a4fee5fc9bad
Shehan Chanuka <shehanxdev@gmail.com>
2023-04-05 08:09:37 +0530

    feat: Added base files for backend and the frontend

---
+++ Backend/src/main/java/com/API/foodiesAPI/FoodiesApiApplication.java
@@ -0,0 +1,13 @@
+package com.API.foodiesAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodiesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodiesApiApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/API/foodiesAPI/FoodiesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.API.foodiesAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodiesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
