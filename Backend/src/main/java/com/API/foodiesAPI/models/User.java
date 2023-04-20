6ac7d8c5dbaa63cb7b91b04d22673385ac321394
Shehan Chanuka <shehanxdev@gmail.com>
2023-04-20 06:56:15 +0530

    feat: added getters and setters for user model

--- Backend/src/main/java/com/API/foodiesAPI/models/User.java
+++ Backend/src/main/java/com/API/foodiesAPI/models/User.java
@@ -17,6 +17,54 @@ public class User {
     private Date createdAt;
     private Date updatedAt;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
     public User(String name, String email, String password) {
         this.name = name;
         this.email = email;
@@ -25,7 +73,6 @@ public class User {
         this.updatedAt = new Date();
     }
 
-    // Getters and setters
-    // ...
+
 
 }
