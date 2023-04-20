48a7aaecf9e0bc3ae084f5c2540b6c5b25a5aee5
Shehan Chanuka <shehanxdev@gmail.com>
2023-04-20 16:55:07 +0530

    feat: controllers, services and repository for notification

---
+++ Backend/src/main/java/com/API/foodiesAPI/controllers/NotificationController.java
@@ -0,0 +1,26 @@
+package com.API.foodiesAPI.controllers;
+
+import com.API.foodiesAPI.models.Notification;
+import com.API.foodiesAPI.services.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/notifications")
+public class NotificationController {
+
+    @Autowired
+    private NotificationService notificationService;
+
+    @GetMapping("/{receiverId}")
+    public List<Notification> getNotificationsByReceiverId(@PathVariable String receiverId) {
+        return notificationService.getNotificationsByReceiverId(receiverId);
+    }
+
+    @PostMapping("/")
+    public Notification addNotification(@RequestBody Notification notification) {
+        return notificationService.addNotification(notification);
+    }
+}
