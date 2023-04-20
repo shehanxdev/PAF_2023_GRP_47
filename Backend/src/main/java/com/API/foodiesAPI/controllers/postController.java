7cbc8d20fb80ed7cdebbcba84a6a8128c9c504ae
it20211196 <it20211196@my.sliit.lk>
2023-04-20 17:37:10 +0530

    feat: added controller service and repo for post endpoints

---
+++ Backend/src/main/java/com/API/foodiesAPI/controllers/postController.java
@@ -0,0 +1,80 @@
+package com.API.foodiesAPI.controllers;
+
+import com.API.foodiesAPI.models.Post;
+import com.API.foodiesAPI.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping()
+public class postController {
+
+    @Autowired
+    private PostService postservice;
+    @CrossOrigin(origins = "http://localhost:8000")
+    @GetMapping("/health")
+    public String healthCheck(){
+        return "Health Check Complete";
+    }
+
+    @CrossOrigin(origins = "http://localhost:8000")
+    @PostMapping
+    public ResponseEntity<?> createStudentInquiry(@RequestBody Post post) {
+        postservice.createPost(post);
+        return ResponseEntity.ok("Post Added");
+    }
+    @CrossOrigin(origins = "http://localhost:8000")
+    @GetMapping
+    public ResponseEntity<List<Map<String, Object>>> getAllPosts() {
+        List<Post> posts = postservice.getAllPosts();
+        List<Map<String, Object>> response = posts.stream().map(post -> {
+            Map<String, Object> postMap = new HashMap<>();
+            postMap.put("id", post.getId().toString());
+            postMap.put("images", post.getImages());
+            postMap.put("description", post.getDescription());
+            postMap.put("authorId", post.getAuthorId());
+            postMap.put("date", post.getDate());
+            postMap.put("comments", post.getComments());
+            postMap.put("likes", post.getLikes());
+            return postMap;
+        }).collect(Collectors.toList());
+        return ResponseEntity.ok(response);
+    }
+
+    @CrossOrigin(origins = "http://localhost:8000")
+    @GetMapping("/{id}")
+    public ResponseEntity<Post> getStudentInquiryById(@PathVariable String id) {
+        Post post = postservice.getPostById(id);
+        if (post != null) {
+            return ResponseEntity.ok(post);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @CrossOrigin(origins = "http://localhost:8000")
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updatePost(@PathVariable String id, @RequestBody Post post) {
+
+   post.setId(id);
+   postservice.updatePost(post);
+        return ResponseEntity.ok().build();
+    }
+    @CrossOrigin(origins = "http://localhost:8000")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteStudentInquiry(@PathVariable(value = "id") String Id) {
+        try {
+            postservice.deletePost(Id);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+ 
+    
+}
---
+++ Backend/src/main/java/com/API/foodiesAPI/service/PostService.java
@@ -0,0 +1,47 @@
+package com.API.foodiesAPI.service;
+import com.API.foodiesAPI.models.Post;
+import com.API.foodiesAPI.repository.PostRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+
+public class PostService {
+    @Autowired
+    private PostRepository postrepository;
+
+    public List<Post> getAllPosts() {
+        return postrepository.findAll();
+    }
+
+    public Post getPostById(String id) {
+        return postrepository.findById(id).orElse(null);
+    }
+
+    public Post createPost(Post post) {
+        return postrepository.save(post);
+    }
+
+    public Post updatePost( Post post) {
+        Post existingPost = postrepository.findById(post.getId().toString()).orElse(null);
+        
+        if (existingPost != null) {
+            existingPost.setImages(post.getImages());
+            existingPost.setDescription(post.getDescription());
+            existingPost.setAuthorId(post.getAuthorId());
+            existingPost.setDate(post.getDate());
+            existingPost.setComments(post.getComments());
+            existingPost.setLikes(post.getLikes());
+            return postrepository.save(existingPost);
+        }
+        return null;
+    }
+
+    public void deletePost(String id) {
+        postrepository.deleteById(id);
+    } 
+    
+}
