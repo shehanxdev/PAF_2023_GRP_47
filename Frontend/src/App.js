03905585a13544971cb22f90291e29fd7fb3ab00
SewwandiDinu <dinushikadodanthenna@gmail.com>
2023-05-12 02:53:34 +0530

    Comment_post css

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
-
 import { BrowserRouter } from "react-router-dom";
 import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
+import Post from "./screens/commentComponent/Post";
+
+
 
 function App() {
   return (
@@ -10,7 +12,9 @@ function App() {
       <Routes>
         <Route path="/" element={<h1>Hello</h1>} />
       </Routes>
+      <div><Post/> </div>
     </BrowserRouter>
+   
   );
 }
 
