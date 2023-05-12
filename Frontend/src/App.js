3a34caa4dff1366eb1160f7bd414deadcb17a20f
Shehan Chanuka <shehanxdev@gmail.com>
2023-05-12 11:55:39 +0530

    refactor: moved comment component inside the relavant screen

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,19 +2,15 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
-import Post from "./screens/commentComponent/Post";
-
-
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<h1>Hello</h1>} />
+        {/* TODO:: Add your routes here */}
       </Routes>
-      <div><Post/> </div>
     </BrowserRouter>
-   
   );
 }
 
